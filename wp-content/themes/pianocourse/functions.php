b29f1c33d55c7e3c950e56508e3ea98c1efd0395
James Grubb <jamesgrubb@mac.com>
2011-11-05 07:57:24 +0000

    added query in functions

--- wp-content/themes/pianocourse/functions.php
+++ wp-content/themes/pianocourse/functions.php
@@ -37,5 +37,11 @@ add_theme_support( 'menus' );
 	add_filter('body_class', 'category_id_class');
 
 
+if( !is_admin()){
+   wp_deregister_script('jquery'); 
+   wp_register_script('jquery', ("http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"), false, '1.3.2'); 
+   wp_enqueue_script('jquery');
+}
+
 
 ?>
