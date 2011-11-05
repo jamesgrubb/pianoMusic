8053089e8f3aa7787a1dc5e150d2a57b2ef6e3a0
James Grubb <jamesgrubb@mac.com>
2011-11-05 11:13:05 +0000

    added class to accordion li

--- wp-content/themes/pianocourse/functions.php
+++ wp-content/themes/pianocourse/functions.php
@@ -26,15 +26,6 @@ add_theme_support( 'menus' );
 
 
 
-// add category nicenames in body and post class
-	function category_id_class($classes) {
-	    global $post;
-	    foreach((get_the_category($post->ID)) as $category)
-	        $classes[] = $category->category_nicename;
-	        return $classes;
-	}
-	add_filter('post_class', 'category_id_class');
-	add_filter('body_class', 'category_id_class');
 
 
 if( !is_admin()){
